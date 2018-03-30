d1905a6b4b451ef9adfbe6c8a72a8191550f42fc
Eric Martineau <ericm@infusionsoft.com>
2018-03-30 09:06:26 -0700

    React 16.0

--- src/Form/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './Button';
-export { default as Checkbox } from './Checkbox';
-export { default as Text } from './Text';
-export { default as Select } from './Select';
-export { default as Wrapper } from './Wrapper';
